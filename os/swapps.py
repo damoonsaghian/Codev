38971265285747f279c12ef2aa2df10fdfb751eb
Damoon Saghian <damoonsaghian84@gmail.com>
2022-06-14 15:21:01 +0430

    Add files via upload

---
+++ os/swapps.py
@@ -0,0 +1,5 @@
+# https://github.com/Biont/sway-launcher-desktop
+# https://github.com/Ulauncher/Ulauncher/
+# https://github.com/DorianRudolph/sirula
+
+# foot --config=/usr/local/share/foot.ini tmux
