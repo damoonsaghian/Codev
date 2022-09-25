42f00efedc4e8864c213969ee80bb7ba715087a2
Damoon Saghian <damoonsaghian84@gmail.com>
2022-09-25 12:54:01 +0330

    Add files via upload

--- comshell-py/overview.py
+++ comshell-py/overview.py
@@ -3,8 +3,21 @@ from gi.repository import Gtk, Gdk, Gio, GLib
 from project import Project
 
 # https://docs.gtk.org/gio/class.VolumeMonitor.html
+#
+# http://storaged.org/doc/udisks2-api/latest/gdbus-org.freedesktop.UDisks2.Drive.html#gdbus-property-org-freedesktop-UDisks2-Drive.Removable
+#
 # if the device is removale and not formated with VFAT/exFAT, ask the user if she wants to format it
+#   format with VFAT (or exFAT if there would be files bigger than 4GB)
+#   http://storaged.org/doc/udisks2-api/latest/gdbus-org.freedesktop.UDisks2.Block.html#gdbus-method-org-freedesktop-UDisks2-Block.Format
 # if the device is not removale and not formated with BTRFS, ask the user if she wants to format it
+#   pkexec sh /usr/local/share/sd-internal.sh format "$1"
+#
+# mount
+# if removable: udisksctl mount -b /dev/"$1"
+# if not removable: pkexec sh /usr/local/share/sd-internal.sh mount "$1"
+#
+# unmount
+# udisksctl unmount -b /dev/"$1"
 
 # after mounting: codev backup mount_path
 
