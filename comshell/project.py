40c278e41e8231d48b9dc7bb82d742384b6fc859
Damoon Saghian <damoonsaghian84@gmail.com>
2023-02-16 17:13:56 +0330

    Add files via upload

---
+++ comshell/project.py
@@ -0,0 +1,22 @@
+from gi.repository import GLib, Gio, Gdk, Gtk
+
+from files import Files
+from editor import Editor
+from gallery import Gallery
+
+class Project:
+  main_view
+  overlay
+  container
+
+  __init__():
+    this.main_view = Gtk.Stack()
+
+    this.overlay = Gtk.Stack()
+
+    this.container = Gtk.Overlay()
+    this.container.add(this.main_view)
+    this.container.add_overlay(this.overlay)
+
+    # if there is a saved session file for the project, restore it
+    # if there is no view, open a view showing the project's files
