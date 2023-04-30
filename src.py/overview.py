05a9467f51f8b0e003985cb54d632e763893072f
Damoon Saghian <damoonsaghian84@gmail.com>
2023-04-30 15:46:23 +0330

    Add files via upload

--- src.py/overview.py
+++ src.py/overview.py
@@ -7,30 +7,27 @@ from project import Project
 # attached devices:
 # https://docs.gtk.org/gio/class.VolumeMonitor.html
 #
-# if a device is not formated, ask the user if she wants to format it
+# ask the user if she wants to format the device, if:
+# , it's not formated
+# , it's a non'system device whose format is not vfat/exfat
+# , it's a system device whose format is not btrfs
+# http://storaged.org/doc/udisks2-api/latest/gdbus-org.freedesktop.UDisks2.Block.html#gdbus-property-org-freedesktop-UDisks2-Block.HintSystem
+#
 # to format it get the volume identifier
 # https://docs.gtk.org/gio/iface.Volume.html
-# use udisks to format it
+# use udisks to format non'system devices with vfat or exfat (for files bigger than 4GB)
 # http://storaged.org/doc/udisks2-api/latest/index.html
 # https://lazka.github.io/pgi-docs/#Gio-2.0/classes/DBusConnection.html
 # https://gjs.guide/guides/gio/dbus.html#direct-calls
 # https://gjs.guide/guides/glib/gvariant.html#basic-usage
-# format types:
-# , removable: vfat
-# , removable containing files bigger than 4GB: exfat
-# , internal: btrfs
+# for system devices: sudo mkfs.btrfs
 #
 # projects on VFAT/exFAT formated devices will be opened as read'only
 # when you try to edit them, you will be asked to copy them on to a BTRFS formated device
 #
 # mount it (if it's not)
-# https://docs.gtk.org/gio/method.Volume.mount.html
-# the device will be mounted in ~/.local/sd-mount/$device_name
-# to unmount:
-# ; sd unmount $device_name
-#
-# if it's a system device use sudo to mount anf format it
-# http://storaged.org/doc/udisks2-api/latest/gdbus-org.freedesktop.UDisks2.Block.html#gdbus-property-org-freedesktop-UDisks2-Block.HintSystem
+# for non'system devices: https://docs.gtk.org/gio/method.Volume.mount.html
+# for system device: sudo mount
 
 class ProjectsList:
 	project_dir
