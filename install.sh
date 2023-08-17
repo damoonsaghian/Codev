d1681e434da98cd7e21a2ec63c702843a82d2631
Damoon Saghian <damoonsaghian84@gmail.com>
2023-08-17 21:37:39 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,54 +1,138 @@
-apt-get install dosfstools exfatprogs btrfs-progs udisks2 libarchive-tools luakit mpv gnunet emacs \
-	libjxl-gdk-pixbuf libavif-gdk-pixbuf webp-pixbuf-loader gstreamer1.0-plugins-ugly gstreamer1.0-libav
-# libav is needed till
-# , h264(openh264), h265(libde265), and aac(fdk-aac) go into plugins-ugly
-# , and av1(aom-libs) goes into plugins-good
+set -e
 
-mkdir -p /usr/local/share/comacs
-cp -r "$(dirname "$0")/comacs" "/usr/local/share/emacs/"
+arch="$(dpkg --print-architecture)"
+case "$arch" in
+s390x|mipsel|mips64el) echo "arichitecture \"$arch\" is not supported"; exit ;;
+esac
 
-first_user="$(id -un 1000)"
-usermod -aG gnunet "$first_user"
-su -c "touch \"/home/$first_user/.config/gnunet.conf\"" "$first_user"
-echo -n '[ats]
-WLAN_QUOTA_IN = unlimited
-WLAN_QUOTA_OUT = unlimited
-WAN_QUOTA_IN = unlimited
-WAN_QUOTA_OUT = unlimited
-' >> "/home/$first_user/.config/gnunet.conf"
-
-# for now just install Comacs, until the real Codev is ready
-exit 0
-
-command -v jina > /dev/null 2>&1 || apt-get install jina || {
-	echo 'to build Codev, "jina" must be installed on the system'
+command -v fzy > /dev/null 2>&1 || apt-get -qq install fzy || {
+	echo "internet connection is required"
 	exit 1
 }
-apt-get install dosfstools exfatprogs btrfs-progs udisks2 libarchive13 libwpewebkit-1.1-0 gnunet
+command -v debootstrap > /dev/null 2>&1 || apt-get -qq install debootstrap
+command -v arch-chroot > /dev/null 2>&1 || apt-get -qq install arch-install-scripts
 
-project_dir="$(dirname "$0")"
-jina build "$project_dir"
-cp "$project_dir/.cache/jina/codev" /usr/local/bin/
+umount --recursive --quiet /mnt || true
 
-mkdir -p /usr/local/share/applications
-cat <<-__EOF__ > /usr/local/share/applications/codev.desktop
-[Desktop Entry]
-Type=Application
-Name=Codev
-Icon=codev
-Exec=/usr/local/bin/codev
-StartupNotify=true
+answer="$(printf "install a new system\nrepair an existing system" | fzy -p "select an option: ")"
+
+[ "$answer" = "repair an existing system" ] && {
+	target_device="$(lsblk --nodep --noheadings -o NAME,SIZE,MODEL | 
+		fzy -p "select the device containing the system: " | cut -d " " -f 1)"
+	
+	target_partitions="$(lsblk --list --noheadings -o PATH "/dev/$target_device")"
+	target_partition1="$(echo "$target_partitions" | sed -n '2p')"
+	target_partition2="$(echo "$target_partitions" | sed -n '3p')"
+	
+	mount "$target_partition2" /mnt
+	if [ -d /sys/firmware/efi ]; then
+		mkdir -p /mnt/boot/efi
+		mount "$target_partition1" /mnt/boot/efi
+	else
+		case "$arch" in
+		amd64|i386) ;;
+		ppc64el) ;;
+		*) mkdir /mnt/boot; mount "$target_partition1" /mnt/boot ;;
+		esac
+	fi
+	
+	arch-chroot /mnt apt-get dist-upgrade || {
+		backup_dir_path="/mnt/backup$(date '+%s')"
+		mkdir "$backup_dir_path"
+		mv /mnt/* "$backup_dir_path/"
+		debootstrap --variant=minbase --include="init,btrfs-progs,udev,netbase,ca-certificates,usr-is-merged" \
+			--components=main,contrib,non-free-firmware stable /mnt
+	}
+	
+	genfstab -U /mnt > /mnt/etc/fstab
+	mount --bind "$(dirname "$0")" /mnt/mnt
+	arch-chroot /mnt sh /mnt/os/install-chroot.sh
+	
+	echo; echo -n "the system on \"$target_device\" repaired successfully"
+	answer="$(printf "no\nyes" | fzy -p "reboot the system? ")"
+	[ "$answer" = yes ] && systemctl reboot
+	exit
+}
+
+target_device="$(lsblk --nodep --noheadings -o NAME,SIZE,MODEL | fzy -p "select a device: " | cut -d " " -f 1)"
+answer="$(printf "no\nyes" | fzy -p "WARNING! all the data on \"$target_device\" will be erased; continue? ")"
+[ "$answer" = yes ] || exit
+
+# create partitions
+if [ -d /sys/firmware/efi ]; then
+	first_part_type=uefi
+	first_part_size="512M"
+	part_label=gpt
+else
+	case "$arch" in
+	amd64|i386)
+		first_part_type="21686148-6449-6E6F-744E-656564454649"
+		first_part_size="1M"
+		part_label=gpt
+		;;
+ 	ppc64el)
+		first_part_type="41,*"
+		first_part_size="1M"
+		part_label=dos
+		;;
+	*)
+		first_part_type="linux,*"
+		first_part_size="512M"
+		part_label=dos
+		;;
+	esac
+fi
+
+second_partition_type=linux
+case "$arch" in
+amd64) second_partition_type=4f68bce3-e8cd-4db1-96e7-fbcaf984b709 ;;
+i386) second_partition_type=44479540-f297-41b2-9af7-d131d5f0458a ;;
+arm64) second_partition_type=b921b045-1df0-41c3-af44-4c6f280d3fae ;;
+armel|armhf) second_partition_type=69dad710-2ce4-4e3c-b16c-21a1d49abed3 ;;
+ppc64el) second_partition_type=c31c45e6-3f39-412e-80fb-4809c4980599 ;;
+riscv64) second_partition_type=72ec70a6-cf74-40e6-bd49-4bda08e8f224 ;;
+esac
+
+command -v sfdisk > /dev/null 2>&1 || apt-get -qq install fdisk
+sfdisk --quiet --wipe always --label $part_label "/dev/$target_device" <<__EOF__
+1M,$first_part_size,$first_part_type
+,,$second_partition_type
 __EOF__
 
-cat <<-__EOF__ > /usr/local/share/icons/hicolor/scalable/apps/codev.svg
-<?xml version="1.0" encoding="UTF-8"?>
-<svg width="64" height="64">
-	<rect style="fill:#dddddd" width="56" height="48" x="4" y="8"/>
-	<rect style="fill:#aaaaaa" width="16" height="48" x="4" y="8"/>
-	<path style="fill:none;stroke:#555555;stroke-width:2;stroke-linecap" d="M 25,14 H 41"/>
-	<path style="fill:none;stroke:#555555;stroke-width:2;stroke-linecap" d="M 30,23 H 48"/>
-	<path style="fill:none;stroke:#555555;stroke-width:2;stroke-linecap" d="M 30,32 H 48"/>
-	<path style="fill:none;stroke:#555555;stroke-width:2;stroke-linecap" d="M 30,41 H 48"/>
-	<path style="fill:none;stroke:#555555;stroke-width:2;stroke-linecap" d="M 25,50 H 41"/>
-</svg>
-__EOF__
+target_partitions="$(lsblk --list --noheadings -o PATH "/dev/$target_device")"
+target_partition1="$(echo "$target_partitions" | sed -n '2p')"
+target_partition2="$(echo "$target_partitions" | sed -n '3p')"
+
+# format and mount partitions
+mkfs.btrfs -f --quiet "$target_partition2" > /dev/null 2>&1
+mount "$target_partition2" /mnt
+if [ -d /sys/firmware/efi ]; then
+	mkfs.fat -F 32 "$target_partition1" > /dev/null 2>&1
+	mkdir -p /mnt/boot/efi
+	mount "$target_partition1" /mnt/boot/efi
+else
+	case "$arch" in
+	amd64|i386) ;;
+	ppc64el) ;;
+	*)
+		mkfs.ext2 "$target_partition1" > /dev/null 2>&1
+		mkdir /mnt/boot
+		mount "$target_partition1" /mnt/boot
+		;;
+	esac
+fi
+
+debootstrap --variant=minbase --include="init,btrfs-progs,udev,netbase,ca-certificates,usr-is-merged" \
+	--components=main,contrib,non-free-firmware stable /mnt
+# "usr-is-merged" is installed to avoid installing "usrmerge" (as a dependency for init-system-helpers)
+
+genfstab -U /mnt > /mnt/etc/fstab
+mount --bind "$(dirname "$0")" /mnt/mnt
+arch-chroot /mnt sh /mnt/os/install-chroot.sh
+
+# arch-chroot copies /media into the new system; it must not; so:
+rm -r /mnt/media/*
+
+echo; echo -n "installation completed successfully"
+answer="$(printf "no\nyes" | fzy -p "reboot the system? ")"
+[ "$answer" = yes ] && systemctl reboot
