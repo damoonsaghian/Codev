f7687e69e938fa15f999eb5a25608090302be8a8
Damoon Saghian <damoonsaghian84@gmail.com>
2024-05-30 00:03:24 +0330

    Add files via upload

--- codev/__main__.py
+++ codev/__main__.py
@@ -1,14 +1,18 @@
-init = {
-	project'views = gui.Stack()
+import gi
+gi.require_version("Gdk", "4.0")
+gi.require_version("Gtk", "4.0")
+from gi.repository import Gio, Gdk, Gtk
+
+project_views = Gtk.Stack()
+
+overview = Overview(project_views)
+
+main_view = Gtk.Overlay()
+main_view.add(project_views)
+main_view.add_overlay(overview)
 	
-	overview = Overview project'views
-	
-	main'view = gui.Overlay()
-	main'view.add project'views
-	main'view.add'overlay overview
-	
-	;; keybinding to show the overview
-	
-	win = gui.Window()
-	win.add main'view
-}
+# keybinding to show the overview
+
+win = Gtk.Window()
+win.add(main_view)
+# when Codev's window is focused: swaymsg "[floating] move scratchpad"
---
+++ codev/files.py
@@ -0,0 +1,26 @@
+# https://stackoverflow.com/questions/23433819/creating-a-simple-file-browser-using-python-and-gtktreeview
+# https://github.com/tchx84/Portfolio
+# http://zetcode.com/gui/pygtk/advancedwidgets/
+# https://github.com/MeanEYE/Sunflower
+# https://gitlab.xfce.org/apps/catfish/
+# https://gitlab.gnome.org/aviwad/organizer
+
+# https://github.com/donadigo/elementary-ide
+
+# archives:
+# bsdtar -xf <file-path>
+
+# .iso file: ask if user wants to extract it, if not, ask for a device to write it into, then:
+# ; sudo dd if=isofile of=devicename
+
+class Files():
+	def __init__(project_directory)
+		widget = Gtk.Listbox()
+	
+	def move_up():
+
+	def move_down():
+
+	def go_to_file():
+
+	def find_file():
