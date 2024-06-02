997fd35af4c0a62e5cd87440223d586c822f38a7
Damoon Saghian <damoonsaghian84@gmail.com>
2024-06-02 22:37:21 +0330

    Add files via upload

--- codev/__main__.py
+++ codev/__main__.py
@@ -18,6 +18,8 @@ win.add(main_view)
 
 # when window is unfocused, make it insensitive
 
-# when window is focused:
-# , make it sensitive again
-# , sh /usr/local/share/swapps.sh
+# when window is focused, make it sensitive again, then:
+# swaymsg "[workspace=__focused__ floating] focus" && {
+# 	swaymsg "[workspace=codev floating] move scratchpad; [app_id=swapps] move scratchpad;
+# 		[app_id=codev] move workspace codev; workspace codev; [app_id=codev] focus"
+# }
