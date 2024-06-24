9cb563f0fc7f3eb9fbcbe09a91e7747deb4033f8
Damoon Saghian <damoonsaghian84@gmail.com>
2024-06-24 17:50:15 +0330

    Add files via upload

--- os/install-chroot.sh
+++ os/install-chroot.sh
@@ -180,7 +180,7 @@ echo -n '<?xml version="1.0"?>
 ' > /etc/fonts/local.conf
 
 apt-get -qq install sway swayidle xwayland lua5.3 lua-lgi gir1.2-gtk-4.0 gnome-console
-cp /mnt/os/{sway.conf,swapps.lua} /usr/local/share/
+cp /mnt/os/{sway.conf,swapps.py} /usr/local/share/
 # the following is to prevent normal users from changing Sway's config
 # this means that, swayidle can't be disabled by a normal user (see sway.conf)
 echo -n '# run sway (if this script is not called by root or a display manager, and this is the first tty)
