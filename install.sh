4d4d350e7d6f1b83557a4711323aa16462d6b83a
Damoon Saghian <damoonsaghian84@gmail.com>
2024-06-24 17:49:20 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 if command -v dpkg 1>/dev/null; then
 	spm add "$(dirname "$(realpath $0)")" \
-		"lua5.3, lua-lgi, gir1.2-gtk-4.0, gir1.2-gtksource-5, gir1.2-webkit-6.0, gir1.2-poppler-0.18, \
+		"python3-gi, gir1.2-gtk-4.0, gir1.2-gtksource-5, gir1.2-webkit-6.0, gir1.2-poppler-0.18, \
 		libavif-gdk-pixbuf, webp-pixbuf-loader, librsvg2-common, \
 		gir1.2-gstreamer-1.0, gstreamer1.0-pipewire, \
 		libgtk-4-media-gstreamer, gstreamer1.0-plugins-good, gstreamer1.0-plugins-ugly, gstreamer1.0-libav, \
 		libarchive-tools, gvfs, dosfstools, exfatprogs, btrfs-progs, gnunet"
 	echo "required packages:"
-	echo "\tlua5.3 lua-lgi gir1.2-gtk-4.0 gir1.2-gtksource-5 gir1.2-webkit-6.0 gir1.2-poppler-0.18"
+	echo "\tpython3-gi gir1.2-gtk-4.0 gir1.2-gtksource-5 gir1.2-webkit-6.0 gir1.2-poppler-0.18"
 	echo "\tlibavif-gdk-pixbuf webp-pixbuf-loader librsvg2-common"
 	echo "\tgir1.2-gstreamer-1.0 gstreamer1.0-pipewire"
 	echo "\tlibgtk-4-media-gstreamer gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-libav"
@@ -27,22 +27,8 @@ mkdir -p "$project_dir/.cache/spm"
 
 ln "$project_dir"/codev/* "$project_dir"/.cache/spm/
 
-cat <<-'__EOF__' > "$project_dir/.cache/spm/0"
-#!/usr/bin/sh
-this_dir = "$(dirname "$(realpath "$0")")"
-args="$(printf "'%s', " "$@")"
-find "$this_dir" -name '?*.lua' -type f -printf "%P\n" | lua5.3 -e "
-	for lua_file_relative_path in io:lines() do
-		local dir_part = string.gsub(lua_file_relative_path, '/[^/]+$', '')
-		local env = _G
-		for subdir in string.gmatch(dir_part, '[^/]+') do
-			env[subdir] = {}
-			env = env[subdir]
-		end
-		loadfile('$this_dir'..'/'..lua_file_relative_path, 'bt', env)()
-	end
-	main($args)
-"
-__EOF__
-chmod +x "$project_dir/.cache/spm/0"
-
+echo '#!/bin/sh
+this_dir="$(dirname "$(realpath "$0")")"
+exec python3 "$this_dir"
+' > "$project_dir"/.cache/spm/0
+chmod +x "$project_dir"/.cache/spm/0
