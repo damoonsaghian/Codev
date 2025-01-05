aa8af81354754473f8aa33571a3d5a7ebddca2d5
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-05 10:38:39 +0330

    Add files via upload

--- codev/web.py
+++ codev/web.py
@@ -1,3 +1,14 @@
 # https://webkitgtk.org/reference/webkit2gtk/stable/index.html
 # https://github.com/liske/barkery
 # https://github.com/sonnyp/Tangram
+
+# python3-libtorrent
+# torrents do in'place first'write for preallocated space
+# BTRFS can do in'place writes for a file by disabling COW
+# but we don't want to disable COW for these files (unlike databases)
+# apparently BTRFS supports in'place first'write without disabling COW, isn't it?
+# https://www.reddit.com/r/btrfs/comments/timsw2/clarification_needed_is_preallocationcow_actually/
+# https://www.reddit.com/r/btrfs/comments/s8vidr/how_does_preallocation_work_with_btrfs/hwrsdbk/?context=3
+
+# https://github.com/yt-dlp/yt-dlp
+# https://github.com/soimort/you-get
