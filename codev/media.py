3d173eef83a10ed95da27430cbd13d47cac62bed
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-05 15:29:43 +0330

    Add files via upload

--- codev/media.py
+++ codev/media.py
@@ -5,5 +5,13 @@
 # use gstreamer pipewire plugin to access camera
 # libaperture, libaravis
 
+# https://wiki.archlinux.org/title/SANE
+# https://github.com/alexpevzner/sane-airscan
+# https://github.com/OpenPrinting/ipp-usb
+
 # audio recording
 # https://gitlab.gnome.org/World/vocalis
+
+# generated media formats
+# MP4/Matroska: AV1 Opus
+# JPEG XL
