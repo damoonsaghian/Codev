d02193560018c958ddea713e0179bcc18f304eaa
Damoon Saghian <damoonsaghian84@gmail.com>
2025-11-26 13:41:51 +0330

    Add files via upload

--- codev-util/sd.sh
+++ codev-util/sd.sh
@@ -1,7 +1,19 @@
 # mounting and formatting storage devices
 
+script_dir="$(dirname "$(realpath "$0")")"
+
 # mount with suid bits disabled
 # mount to ~/.local/state/mounts
+
+# if queued trim is supported, use discard option when mounting
+if [ "$(cat /sys/block/"$device"/queue/discard_granularity)" -gt 0 ] &&
+	[ "$(cat /sys/block/"$device"/queue/discard_max_bytes)" -gt 2147483648 ]
+then
+fi
+
+# before unmount, run "fstrim <mount-point>" for devices supporting unqueued trim
+# [ "$(cat /sys/block/"$device"/queue/discard_granularity)" -gt 0 ] &&
+# [ "$(cat /sys/block/"$device"/queue/discard_max_bytes)" -lt 2147483648 ] &&
 
 # exit if it's the system device
 
@@ -11,3 +23,5 @@
 # format non'system devices, format with vfat or exfat (if wants files bigger than 4GB)
 # for system devices:
 # doas sh -c "mkfs.btrfs -f <dev-path>; mount <dev-path> /mnt; chmod 777 /mnt; umount /mnt"
+
+[ "$1" = new-sys ] && . "$script_dir"/sd-new-sys
---
+++ codev-util/tpm-get-luks-key.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# use /boot/pcrsig (in efi partition) to unseal the luks key
+# tpm2_unseal -H 0x81000000 -L sha1:4,9,12
+
+# load signer public key to the tpm
+tpm2_loadexternal -G rsa -C o -u signing_key_public.pem -c signing_key.ctx -n signing_key.name
+# verify signature on the PCR policy and generate verification ticket
+tpm2_verifysignature -c signing_key.ctx -g sha256 -m set2.pcr.policy -s set2.pcr.signature -t verification.tkt -f rsassa
+# satisfy PCR policy and authorized policy
+tpm2_startauthsession --policy-session -S session.ctx
+tpm2_policypcr -l sha256:0 -S session.ctx
+tpm2_policyauthorize -S session.ctx -i set2.pcr.policy -n signing_key.name -t verification.tkt
+# unseal the secret and pipe to cryptsetup to open the LUKS encrypted volume
+sudo losetup $loopdevice enc.disk
+tpm2_unseal -p session:session.ctx -c 0x81010001 | sudo cryptsetup luksOpen --key-file=- $loopdevice enc_volume
+tpm2_flushcontext session.ctx
+sudo mount /dev/mapper/enc_volume mountpoint/
+
+# if failed, warn the user that the system is tampered with
+# 	ask the user to enter password only if she is sure that the source of tamper is herself
+
+# if unseal was successful but decryption of root is failed, it means that:
+# , the root is replaced
+# , the key based slot of luks header is corrupted
+# so try other keys
+# if all fails, ask the user to connect a backup storage device
+# then try to decrypt luks devices, and when successful, copy it luks header onto the corrupted one
