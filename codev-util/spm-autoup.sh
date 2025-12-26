06d6447d93c465439a5cd6fab4386d99c44488e0
Damoon Saghian <damoonsaghian84@gmail.com>
2025-12-26 23:01:38 +0330

    Add files via upload

---
+++ codev-util/spm-autoup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# a 5min delay, for when it's started on boot
+sleep 300
+
+metered_connection() {
+	#nmcli --terse --fields GENERAL.METERED dev show | grep --quiet "yes"
+	#dbus: org.freedesktop.NetworkManager Metered
+}
+metered_connection && exit 0
+
+# if plugged
+
+# inhibit suspend/shutdown when an upgrade is in progress
+
+# if during autoupdate an error occures: echo error > /var/cache/autoupdate-status
+
+spm update
+
+# fwupd
+# just check, if available download and show notification in status bar
