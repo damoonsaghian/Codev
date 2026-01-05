88d00ad69bebc65552adf3cd74b2a2b74b5a7b2a
Damoon Saghian <damoonsaghian84@gmail.com>
2026-01-05 23:51:45 +0330

    Add files via upload

--- codev-util/spm-autoup.sh
+++ codev-util/spm-autoup.sh
@@ -3,11 +3,13 @@
 # a 5min delay, for when it's started on boot
 sleep 300
 
-metered="$(dbus-send --system --print-reply=literal --dest=org.freedesktop.NetworkManager \
-	/org/freedesktop/NetworkManager org.freedesktop.DBus.Properties.Get \
-	string:org.freedesktop.NetworkManager string:Metered | )"
-[ "$metered" = NM_METERED_YES ] || [ "$metered" = NM_METERED_GUESS_YES ] && exit 1
+# do not run autoupdate on metered connection
+case nmcli --terse --field METERED general in
+yes*) exit 11 ;;
+esac
 
-# if plugged
+[ -e /sys/class/power_supply/BAT0 ] &&
+	cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=Discharging" &>/dev/null &&
+	exit 11
 
 spm update auto || echo error > /tmp/spm-status
