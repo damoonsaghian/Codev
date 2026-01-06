8a44b49e4cc099d07b3cde578c2e9bbbfe67a8de
Damoon Saghian <damoonsaghian84@gmail.com>
2026-01-06 10:11:13 +0330

    Add files via upload

--- codev-util/spm-autoup.sh
+++ codev-util/spm-autoup.sh
@@ -9,7 +9,8 @@ yes*) exit 11 ;;
 esac
 
 [ -e /sys/class/power_supply/BAT0 ] &&
-	cat /sys/class/power_supply/BAT0/uevent | grep "POWER_SUPPLY_CAPACITY=Discharging" &>/dev/null &&
+	[ "$(cat /sys/class/power_supply/BAT0/status)" = Discharging ] &&
+	[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt 10 ] &&
 	exit 11
 
 spm update auto || echo error > /tmp/spm-status
